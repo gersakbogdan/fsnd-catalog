cadc9923804079d8a2c8f1cedee51d3a2136ad83
Bogdan Gersak <bogdan@gersak.ro>
2015-10-09 04:40:48 +0000

    Populate database

---
+++ db-populate.py
@@ -0,0 +1,12 @@
+from catalog import db
+from catalog.models.category import Category
+
+dessert = Category('Dessert')
+chicken = Category('Chicken')
+main_dish = Category('Main Dish')
+appetizer = Category('Appetizer')
+
+db.session.add(dessert)
+db.session.add(chicken)
+db.session.add(main_dish)
+db.session.add(appetizer)
