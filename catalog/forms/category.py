3b885d79194de0349fa556426a3de6354a497f71
Bogdan Gersak <bogdan@gersak.ro>
2015-10-13 05:01:04 +0000

    Form fields display error state

--- catalog/forms/category.py
+++ catalog/forms/category.py
@@ -7,4 +7,4 @@ from wtforms.validators import Required, Length
 class CategoryForm(Form):
     name = StringField('name', validators=[Required(), Length(min=1, max=20)])
     description = TextAreaField('description', validators=[Required(), Length(min=1, max=500)])
-    image = FileField('image', validators=[FileRequired(), FileAllowed(['jpeg', 'jpg', 'png'], 'Only images are allowed!')])
+    image = FileField('image', validators=[FileAllowed(['jpeg', 'jpg', 'png'], 'Only images are allowed!')])
