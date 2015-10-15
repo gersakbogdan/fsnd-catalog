7997af887a40a7867daa99958036ed1abebc5d26
Bogdan Gersak <bogdan@gersak.ro>
2015-10-15 04:48:11 +0000

    Add comments

--- catalog/helpers/__init__.py
+++ catalog/helpers/__init__.py
@@ -10,6 +10,7 @@ _punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
 
 def slugify(text, delim=u'-'):
     """Generates slug from string."""
+
     result = []
     for word in _punct_re.split(text.lower()):
         word = normalize('NFKD', word).encode('ascii', 'ignore')
@@ -43,6 +44,8 @@ def upload_category_image(imagedata, category_id):
     image_file.save(file_path)
 
 def upload_image(imagedata, save_path, size):
+    """General save upload image meethod."""
+
     # save uploaded file
     ext = imagedata.filename.split('.')[-1]
     # generate a safe name for image
@@ -70,13 +73,20 @@ def delete_recipe_image(filename):
         pass
 
 def csrf_protect():
+    """Check request against CSRF.
+
+    This method compares session token with request token and if there
+    is no match 403 error is returned.
+    """
+
     if request.method == "POST":
         token = session.pop('_csrf_token', None)
-        print token, request.form.get('_csrf_token')
         if not token or str(token) != str(request.form.get('_csrf_token')):
             abort(403)
 
 def generate_csrf_token():
+    """Generates and return CSRF token."""
+
     if '_csrf_token' not in session:
         session['_csrf_token'] = uuid.uuid4()
     return session['_csrf_token']
