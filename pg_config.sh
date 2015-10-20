622d965bd7a05c6c2dd848b43e5d6b0fbb3775a1
Bogdan Gersak <bogdan@gersak.ro>
2015-10-20 11:38:22 +0300

    Update vagrant bootstrap script

--- pg_config.sh
+++ pg_config.sh
@@ -11,10 +11,5 @@ apt-get -qqy install zlib1g-dev
 
 pip install virtualenv
 
-su postgres -c 'createuser -dRS vagrant'
-su vagrant -c 'createdb'
-su vagrant -c 'createdb forum'
-su vagrant -c 'psql forum -f /vagrant/forum/forum.sql'
-
 vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
 echo -e $vagrantTip > /etc/motd
